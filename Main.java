481868f4353827834a1c47c4d83770eadf410beb
Kapil Dixit <kapil_dixit@outlook.com>
2023-06-21 01:16:09 +0530

    Initial Version

---
+++ Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Float> random = new ArrayList<Float>();
+        random.add(2f);
+        random.add(4f);
+        random.add(5f);
+        random.add(10f);
+        random.add(99.9f);
+
+        System.out.println(random.contains(5.0f));
+    }
+}
+
